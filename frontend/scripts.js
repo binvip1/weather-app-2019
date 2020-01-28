5cfa574452faaee64363cd17ab851d67e8f186d8
Minh Duc Pham <minhduc.pham@tuni.fi>
2020-01-28 13:06:00 +0200

    task3

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -14,5 +14,7 @@ const clickmeButton = document.getElementById("clickme");
 clickmeButton.addEventListener('click', clickmeFunction);
 
 /* ------------- Custom JavaScript starts ---------------- */
-
+const currentDate= new Date();
+const currentDateSpan = document.getElementById("current-date");
+currentDateSpan.textContent = currentDate;
 // Write your own code here
