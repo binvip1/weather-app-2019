5bab718baca62ee3830a4021dd9f5755ba506c80
Minh Duc Pham <minhduc.pham@tuni.fi>
2020-04-19 03:30:17 +0700

    pipeline

---
+++ api/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: [
+      'airbnb-base',
+      'plugin:import/errors'
+    ],
+    settings: {
+      'import/resolver': 'node',
+    },
+    env: {
+      es6: true,
+      node: true,
+    },
+    rules: {
+      'no-unused-vars': 1,
+      'no-await-in-loop': 1,
+      'camelcase': 0,
+      'no-console': 0,
+      'prefer-destructuring': 0,
+      'prefer-template': 0,
+      'comma-dangle': 0,
+      'eol-last': 0,
+      'quotes' :0,
+    },
+  };
